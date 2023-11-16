3fcb99ea910610cc805ec9d20eab502fc0ab4ba3
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-16 18:28:15 -0500

    🔧 Update Astro adapter configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    mode: 'directory',
+    functionPerRoute: true,
+    imageService: "passthrough"
+  })
 });
